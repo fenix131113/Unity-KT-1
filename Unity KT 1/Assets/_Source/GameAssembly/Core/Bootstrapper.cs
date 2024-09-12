cdd210fcde8547209d2032e7a7f5b31af837fce4
Алексей <ix.fenix@mail.ru>
2024-09-12 15:30:28 +0300

    feat: code scheme

--- Unity KT 1/Assets/_Source/GameAssembly/Core/Bootstrapper.cs
+++ Unity KT 1/Assets/_Source/GameAssembly/Core/Bootstrapper.cs
@@ -2,14 +2,13 @@ using InputSystem;
 using PlayerSystem;
 using ShootSystem;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Core
 {
 	public class Bootstrapper : MonoBehaviour
 	{
 		[SerializeField] private InputListener inputListener;
-		[FormerlySerializedAs("playerShoot")] [SerializeField] private PlayerCombat playerCombat;
+		[SerializeField] private PlayerCombat playerCombat;
 		[SerializeField] private Player player;
 		
 		private PlayerMovement _playerMovement;
